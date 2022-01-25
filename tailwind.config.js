fbb3fddc472b2a2b7e3d20f5f22daae18d0b51f1
Maneren <49210777+Maneren@users.noreply.github.com>
2022-01-25 09:38:01 +0100

    set up the env with tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+// tailwind.config.js
+const defaultTheme = require('tailwindcss/defaultTheme');
+
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx}',
+    './node_modules/tw-elements/dist/js/**/*.js'
+  ],
+  theme: {
+    extend: {
+      fontFamily: {
+        sans: ['Inter var', ...defaultTheme.fontFamily.sans]
+      }
+    }
+  },
+  plugins: [require('tw-elements/dist/plugin')]
+};
